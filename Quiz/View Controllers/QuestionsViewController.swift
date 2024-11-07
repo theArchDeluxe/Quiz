7c7d03184ec73181381a229f555595bc729eb366
theArchDeluxe <156505949+theArchDeluxe@users.noreply.github.com>
2024-11-07 21:11:13 +0300

    End of lesson

--- Quiz/View Controllers/QuestionsViewController.swift
+++ Quiz/View Controllers/QuestionsViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 final class QuestionsViewController: UIViewController {
     
+    // MARK: - IB Outlets
     @IBOutlet var questionLabel: UILabel!
     @IBOutlet var questionProgressView: UIProgressView!
     
@@ -27,22 +28,111 @@ final class QuestionsViewController: UIViewController {
     
     @IBOutlet var rangedSlider: UISlider!
     
+    // MARK: - Private Properties
+    private var questionIndex = 0
     private let  questions = Question.getQuestions()
-    
-    
+    private var answersChosen: [Answer] = []
+    private var currentAnswers: [Answer] {
+        questions[questionIndex].answers
+    }
+    // MARK: - View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        updateUI()
+        let answersCount = Float(currentAnswers.count - 1)
+        rangedSlider.minimumValue = answersCount
+        rangedSlider.value = answersCount / 2
     }
 
+    // MARK: - IB Actions
     @IBAction func singleQuestionButtonPressed(_ sender: UIButton) {
+        guard let buttonIndex = singleButtons.firstIndex(of: sender) else { return }
+        let currentAnswer = currentAnswers[buttonIndex]
+        answersChosen.append(currentAnswer)
+        nextQuestion()
     }
     
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+    }
     @IBAction func multipleQuestionButtonPressed() {
+        for (multipleSwitch, answer) in zip(multipleSwitches, currentAnswers) {
+            if multipleSwitch.isOn {
+                answersChosen.append(answer)
+            }
+        }
+        nextQuestion()
     }
     
     @IBAction func rangedQuestionButtonPressed() {
+        let index = lrintf(rangedSlider.value)
+        answersChosen.append(currentAnswers[index])
+        nextQuestion()
     }
     
 }
 
+// MARK: - Private Methods
+private extension QuestionsViewController {
+    func updateUI() {
+        for stackView in [singleStackView, multipleStackView, rangedStackView] {
+            stackView?.isHidden = true
+        }
+        
+        title = "Вопрос № \(questionIndex + 1) из \(questions.count)"
+        let currentQuestion = questions[questionIndex]
+        questionLabel.text = currentQuestion.title
+        let totalProgress = Float(questionIndex) / Float(questions.count)
+        questionProgressView.setProgress(totalProgress, animated: true)
+        showCurrentAnswers(for: currentQuestion.type)
+    }
+    
+    /// Choice of answers category
+    ///
+    ///Display answers to a question according to a category
+    ///
+    /// - Parameter type: Specifies the category of response
+    func showCurrentAnswers(for type: ResponseType) {
+        switch type {
+        case .single:
+            showSingleStackView(with: currentAnswers)
+        case .multiple:
+            showMultipleStackView(with: currentAnswers)
+        case .ranged:
+            showRangedStackView(with: currentAnswers)
+        }
+    }
+    func showSingleStackView(with answers: [Answer]) {
+        singleStackView.isHidden.toggle()
+        
+        for (button, answer) in zip(singleButtons, answers) {
+            button.setTitle(answer.title, for: .normal)
+        }
+    }
+    
+    func showMultipleStackView(with answers: [Answer]) {
+        multipleStackView.isHidden.toggle()
+        
+        for (label, answer) in zip(multipleLabels, answers) {
+            label.text = answer.title
+        }
+    }
+    
+    func showRangedStackView(with answers: [Answer]) {
+        rangedStackView.isHidden.toggle()
+        
+        rangedLabels.first?.text = answers.first?.title
+        rangedLabels.last?.text = answers.last?.title
+    }
+    
+    func nextQuestion() {
+        questionIndex += 1
+        
+        if questionIndex < questions.count {
+            updateUI()
+            return
+        }
+        
+        performSegue(withIdentifier: "showResult", sender: nil)
+    }
+}

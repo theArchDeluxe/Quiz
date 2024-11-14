2357ab2cf83587a28e87f2376dbf267f03c7853e
theArchDeluxe <156505949+theArchDeluxe@users.noreply.github.com>
2024-11-14 04:03:01 +0300

    update results

--- Quiz/View Controllers/ResultViewController.swift
+++ Quiz/View Controllers/ResultViewController.swift
@@ -7,23 +7,47 @@
 
 import UIKit
 
-class ResultViewController: UIViewController {
+final class ResultViewController: UIViewController {
 
+    @IBOutlet var animalTypeLabel: UILabel!
+    @IBOutlet var descriptionLabel: UILabel!
+    
+    var answers: [Answer]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        navigationItem.hidesBackButton = true
+        updateResult()
 
-        // Do any additional setup after loading the view.
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func doneButtonAction(_ sender: UIBarButtonItem) {
+        dismiss(animated: true)
     }
-    */
+    
+    deinit {
+        print ("\(type(of: self)) has been deallocated")
+    }
 
+}
+extension ResultViewController {
+    private func updateResult() {
+        var frequencyOfAnimals: [Animal: Int] = [:]
+        let animals = answers.map { $0.animal }
+        
+        for animal in animals {
+            frequencyOfAnimals[animal, default: 0] += 1
+        }
+        
+        let sortedFrequentOfAnimals = frequencyOfAnimals.sorted { $0.value > $1.value }
+        guard let mostFrequentAnimal = sortedFrequentOfAnimals.first?.key else { return }
+        
+        updateUI(with: mostFrequentAnimal)
+    }
+       
+        private func updateUI(with animal: Animal) {
+            animalTypeLabel.text = "Вы - \(animal.rawValue)!"
+            descriptionLabel.text = animal.definition
+        }
 }
